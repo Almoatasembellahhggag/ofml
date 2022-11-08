1c813d0a52744601a6593e1b5abaf7cfda8d4829
Almoatasem Bellah Haggag <almoatasem.bellah.haggag@gmail.com>
2022-11-08 19:50:45 +0100

    ex 1.4
    ex 1.4 done

--- ex1/exercise_1.py
+++ ex1/exercise_1.py
@@ -4,6 +4,8 @@
 # Author: Your Name <your@email.com>
 import numpy as np
 import itertools
+from collections import namedtuple
+
 
 # For exercise 1.2
 def evaluate_energy(nodes, edges, assignment):
@@ -38,12 +40,66 @@ def bruteforce(nodes, edges):
 
 # For exercise 1.4
 def dynamic_programming(nodes, edges):
-    F, ptr = None, None
-    return F, ptr
+    
+    # make sure that i only use the edges that would make the a chain (right = left - 1) 
+    new_edges = [edge for edge in edges if (edge.right - edge.left == 1)]
+    
+    #Processed label is a named tuple (pseudo object) that i created to store the date of each label (bellman costs (F) , Unary cost (Theta) , connected label (to get the back tracing))
+    processed_label = namedtuple('processed_label', 'bellman_cost unary_cost connected_label')
+    
+    #Processed node is a named tuple (pseudo object) that i created to store the list of processed labels in each node)
+    processed_node = namedtuple('processed_node', 'processed_labels')    
+    
+    #list of all processed nodes
+    processed_nodes = []    
+    for index , node in enumerate(nodes):
+        list_of_processed_labels = []
+        #for the first node all the labels in the node will have a bellman function of 0 - given in the lecture - and each label won't be conected to other nodes from the left (because it is the first node in the chain) 
+        if (index == 0):
+            for i,cost in enumerate(node.costs):
+              new_label = processed_label(bellman_cost=0,unary_cost=node.costs[i],connected_label=None)
+              list_of_processed_labels.append(new_label)
+            curr_node = processed_node(processed_labels=list_of_processed_labels)
+            processed_nodes.append(curr_node)
 
-def backtrack(nodes, edges, F, ptr):
-    assignment = [0] * len(nodes)
-    return assignment
+        else:
+            for curr_index,curr_label in enumerate (node.costs):
+                comparision_array = []
+                for prev_index,prev_label in enumerate (processed_nodes[index-1].processed_labels):
+                     # sum unary cost(modified node[0]) and the bellman function (modified node[1]) + the pairwise cost (the corresbonding edge)
+                     value = prev_label.bellman_cost + prev_label.unary_cost + new_edges[index-1].costs[(prev_index,curr_index)]
+                     comparision_array.append(value)
+                     
+                new_label = processed_label(bellman_cost=np.min(comparision_array),unary_cost=curr_label,connected_label=np.argmin(comparision_array))
+                list_of_processed_labels.append(new_label)
+            curr_node = processed_node(processed_labels=list_of_processed_labels) 
+            processed_nodes.append(curr_node)
+
+            if index == len(nodes)-1:
+                last_node_comparision_array =[]
+                for label in curr_node.processed_labels:
+                    last_node_comparision_array.append((label.unary_cost+label.bellman_cost))
+                ptr = np.argmin(last_node_comparision_array)
+                
+
+    return list(reversed(processed_nodes)), ptr
+
+def backtrack(nodes, edges, processed_nodes, ptr):
+    assignment=[ptr]
+    for index,node in enumerate(processed_nodes):
+        next_step = node.processed_labels[assignment[-1]].connected_label
+        if (next_step !=None):
+            assignment.append(next_step)
+        
+
+
+
+        
+
+
+    
+    print(list(reversed(assignment)))
+    return list(reversed(assignment))
 
 
 # For exercise 1.5
